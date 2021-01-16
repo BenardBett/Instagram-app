5ce09a69798713e23f81a1369316434539145d70
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 19:04:56 +0300

    created password confirmation field in the signupform class

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,4 @@ class SignupForm(forms.Form):
     """Signup form"""
     username= forms.CharField(min_length=5,max_length=50,widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
     password = forms.CharField(min_length=8,max_length=70, widget=forms.PasswordInput(attrs={'placeholder': 'Password','class': 'form-control'}))
+    password_confirmation = forms.CharField( min_length=8, max_length=70, widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation','class': 'form-control'}))
