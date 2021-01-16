bc9ac6fe2ef9869177171dac7d8846b828e7ae4c
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 17:07:46 +0300

    created a url path for login

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrb.auth.decorators import login_required
+from posts import views
+
+urlpatterns = [
+    path(
+        route='',
+        view=login_required(views.PostFeedView.as_view()),
+        name='feed'
+    ),
+]
