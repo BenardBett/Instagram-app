dfc2e6feaec566cb3e5491b4d541f9c4580ce195
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 16:53:58 +0300

    created a class that create new post in the model

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+from posts.models import Post
+from posts.forms import PostForm
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create New Post View"""
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+    context_object_name = 'form'
