4348efd7e6c1f59b0fd20faefb0e2c0fe0a4a8a8
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 20:01:20 +0300

    created a url path for login,logout and updating profile

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from django.contrib.auth.decorators import login_required
 from users import views
 
 urlpatterns=[route='signup/', view=views.Signupview.as_view(), name='signup'),
-    
+urlpatterns=[route='login/', view=views.Loginview.as_view(), name='login'),
+urlpatterns=[route='logout/', view=views.Logout.as_view(), name='logout'), 
+urlpatterns=[route='me/profile/', view=views.UpdateProfile.as_view(), name='update_profile'), 
+urlpatterns=[route='<str:username_slug>/', view=login_required(views.UserDetailView.as_view()), name='detail'), 
 ]
