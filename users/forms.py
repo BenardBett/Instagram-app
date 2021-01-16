cf42a6eb2802f6380d67cf2913a495c9c9bc770e
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 19:03:12 +0300

    created password input file in the signupform class

--- users/forms.py
+++ users/forms.py
@@ -5,3 +5,4 @@ from users.models import Profile
 class SignupForm(forms.Form):
     """Signup form"""
     username= forms.CharField(min_length=5,max_length=50,widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
+    password = forms.CharField(min_length=8,max_length=70, widget=forms.PasswordInput(attrs={'placeholder': 'Password','class': 'form-control'}))
