8c28edb887f063d815f7cb8f55438c2d825caa14
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 19:27:32 +0300

    created a method for saving its user and profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth import logout
+from users.forms import SignupForm
+from posts.models import Post
+from users.models import Profile
 # Create your views here.
+
+class SignupView(FormView):
+    """Signup View."""
+    template_name = 'users/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        """If the form is valid save the user"""
+        form.save()
+        return super().form_valid(form)
