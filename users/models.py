6baf9451fe0744629ff10cb5404407e764f8aa4f
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 18:14:13 +0300

    created a class profile in the users profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.model):
+      """
+    Profile model
+
+    Proxy Model that extends the base data with other information.
+    """
+    # Relation con la tabla user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    website= models.URLField(max_length=200, blank=True)
+    biography= models.TextField(blank=True)
+    phone_number= models.CharField(max_length=20, blank=True)
