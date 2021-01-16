07e69807c86039897e8c83893a665e6d5387d32f
BenardBett <kiplangatbett5@gmail.com>
2021-01-16 18:18:50 +0300

    created a field for uploading images

--- users/models.py
+++ users/models.py
@@ -15,3 +15,11 @@ class Profile(models.model):
     website= models.URLField(max_length=200, blank=True)
     biography= models.TextField(blank=True)
     phone_number= models.CharField(max_length=20, blank=True)
+    
+    
+    
+    picture = models.ImageField(
+        upload_to='users/pictures',
+        blank=True,
+        null=True
+    )
